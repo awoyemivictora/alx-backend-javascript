5b8d9cc63ffc5fc12db8d746c127057222056ea4
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-04-18 05:34:50 +0100

    fixing task 1

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,4 @@
 export default function returnHowManyArguments(...theArgs) {
   console.log(theArgs.length);
 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                              
