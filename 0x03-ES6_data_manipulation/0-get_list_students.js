f3184b250e90971d59f44512c0f27141378f8967
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-05-03 17:18:50 +0100

    testing task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,38 @@
 
-// const students = [
-//     { id: 1, firstName: "Guillaume", location: "San Francisco" },
-//     { id: 2, firstName: "James", location: "Columbia" },
-//     { id: 5, firstName: "Serena", location: "San Francisco" },
-//     ],
+const students = [
+    {
+    "id": 1,
+    "firstName": "Guillaume",
+    "location": "San Francisco"
+    },
+    {
+    "id": 2,
+    "firstName": "James",
+    "location": "Columbia"
+    },
+    {
+    "id": 5,
+    "firstName": "Serena",
+    "location": "San Francisco"
+    }
+]
     // res = students.map(getListStudents => getListStudents.id); 
     // console.log(res); 
 
-function getListStudents(id, firstName, location) {
+// function getListStudents(id, firstName, location) {
     
-    var obj = {
-        id: id,
-        firstName: firstName,
-        location: location
-    };
+//     var obj = {
+//         id: id,
+//         firstName: firstName,
+//         location: location
+//     };
 
-    return obj;
-}
+//     return obj;
+// }
 
-var p1 = getListStudents(1, "Guillaume", "San Franscisco")
-console.log(p1);
+// var p1 = getListStudents(1, "Guillaume", "San Franscisco")
+// console.log(p1);
 
+
+const a1 = students.map(getListStudents => getListStudents);
+console.log(a1)
