1463cc951f1fad1938ca68aceae4f5d07facfb26
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-27 23:39:01 +0100

    completed task 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./api');
+
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('Index page', () => {
+    it('should return correct status code and result', (done) => {
+        chai
+            .request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.text).to.equal('Welcome to the payment system');
+                done();
+            });
+    });
+});
