5e651081007dbe0ead465ba98bd13929a8de652d
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-27 23:14:38 +0100

    completed task 6

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+export function getPaymentTokenFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({ data: 'Successful response from the API' });
+        } else {
+            // Do nothing (i.e., the promise remains pending)
+        }
+    });
+}
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { getPaymentTokenFromAPI } from "./6-payment_token.js";
+
+describe('getPaymentTokenFromAPI', () => {
+    it('should return a successful response when success is true', (done) => {
+        getPaymentTokenFromAPI(true)
+            .then((response) => {
+                // Verify that the response contains the expected data
+                expect(response).to.have.property('data').that.equals('Successful response from the API');
+                done(); // Call done to signal that the test is complete
+            })
+            .catch((err) => done(err)); // If there is an error, pass it to done
+    });
+});
