ecc79b540ae367d39ed00f7140ed9db3bebac1b5
Awoyemi Victor <awoyemivictora@icloud.com>
2024-11-28 04:42:21 +0100

    fixing task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,9 +1,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('./api');
+const { expect } = require('chai');
 
-
-const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Index page', () => {
